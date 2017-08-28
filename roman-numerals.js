ffd464fbdc532f73b3ff4ab6ccb04c80631b43ca
jackhowa <johncarverhoward@gmail.com>
2017-08-28 12:03:52 -0500

    Add comments, more concise functions

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,5 +1,6 @@
 
 // make a conversion key with arabic to roman
+// will use keys and values with dictionary
 var romanConversion = {
   M: 1000,
   CM: 900,
@@ -16,38 +17,66 @@ var romanConversion = {
   I: 1
 };
 
+// input number like 34
+// output XXXIV
 function convertToRoman(num) {
-  var totalOutput = "";
+  // set output for the whole number
+  var romanOutput = "";
 
+  // loop through while condition is true
+  // the input could be zero or below so
+  // wouldn't even want to run once using do while if so
   while (num > 0) {
-    totalOutput += addNumerals(num);
+
+    // use add numerals for each letter each time num
+    // runs through the while loop
+    romanOutput += addNumerals(num);
+
+    // num is the remainder of what's left in num
+    // modulo equals sign ensures num will go through
+    // while loop again accurately
     num %= findHighestMultiple(num);
   }
 
-  return totalOutput;
+  // return output for whole number
+  return romanOutput;
 }
 
+// not using output as a variable here because
+// want to keep the functions not dependent on state
 function addNumerals(num) {
-  var letterOutput = '';
+
+  // set total output for each letter
+  var letterOutput = "";
+
+  // i would run four times making iiii if num is 4
+  // find multiple occurence is how many times current number goes into roman number conversion
   for (var i = 0; i < findMultipleOccurence(num); i++) {
     letterOutput += findRoman(num);
   }
+
+  // output for each letter
   return letterOutput;
 }
 
 function findRoman(num) {
-  var highestMultiple = findHighestMultiple(num);
 
-  return Object.keys(romanConversion).find(key => romanConversion[key] === highestMultiple);
+  // roman conversion table keys are the arabic nums
+  // find gives me the first highest number because the dictionary is in descending order
+  return Object.keys(romanConversion).find(key => romanConversion[key] === findHighestMultiple(num));
 }
 
-function findMultipleOccurence(num) {
-  return Math.floor(num / findHighestMultiple(num));
-}
 
 // find greatest multiple for iterating over
+// 110 would find 100 as the highest number
 function findHighestMultiple(num) {
   return romanValues().find(arabicNum => arabicNum <= num);
+}
+
+// figure out how many times match multiple 30 goes into highest multiple like 10
+// would need to thereby iterate 3 times
+function findMultipleOccurence(num) {
+  return Math.floor(num / findHighestMultiple(num));
 }
 
 // returns all of the values of tradition romans
@@ -58,4 +87,4 @@ function romanValues() {
   });
 }
 
-console.log(convertToRoman(36));
+console.log(convertToRoman(34));
