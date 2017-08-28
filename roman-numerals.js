d37fda43145e12620049917f49e55f6cf4ae5336
jackhowa <johncarverhoward@gmail.com>
2017-08-28 10:50:39 -0500

    add dictionary returning variable of values

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,6 +1,25 @@
 
+// make a conversion key with arabic to roman
+var traditionRomans = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1
+};
+
 function convertToRoman(num) {
- return num;
+ console.log(findClosestNumeral(num));
+}
+
+function findClosestNumeral(arabicNum) {
+  // returns all of the values of tradition romans
+  // object.values is not supported by most browsers nor node 
+  return Object.keys(traditionRomans).map(function(key) {
+    return traditionRomans[key];
+  });
 }
 
 convertToRoman(36);
